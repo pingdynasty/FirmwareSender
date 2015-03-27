c5b9d07c0e7d28eff1d49944dc185b62bb18fba9
mars <martin@rebel-it.com>
2015-03-27 17:27:40 +0000

    build updates

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -14,6 +14,11 @@
 #define __APPHEADERFILE_BYWD93__
 
 #include "AppConfig.h"
+#include "modules/juce_audio_basics/juce_audio_basics.h"
+#include "modules/juce_audio_devices/juce_audio_devices.h"
+#include "modules/juce_audio_formats/juce_audio_formats.h"
+#include "modules/juce_core/juce_core.h"
+#include "modules/juce_events/juce_events.h"
 
 #if ! DONT_SET_USING_JUCE_NAMESPACE
  // If your code uses a lot of JUCE classes, then this will obviously save you
