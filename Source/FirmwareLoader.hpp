69a9f7cbb7958c0efe9562b42af87d3eb0310220
Martin Klang <martin@rebel-it.com>
2021-07-07 15:50:34 +0200

    updated FirmwareLoader

--- Source/FirmwareLoader.hpp
+++ Source/FirmwareLoader.hpp
@@ -5,32 +5,47 @@
 #include "crc32.h"
 #include "sysex.h"
 // #include "device.h"
+#include <stdint.h>
+// #include "owlcontrol.h"
+// #include "errorhandlers.h"
+// #include "ProgramManager.h"
 
 class FirmwareLoader {
 private:
-  // enum SysexFirmwareStatus {
+  // enum SysExFirmwareStatus {
   //   NORMAL = 0,
   //   UPLOADING,
   //   ERROR = 0xff
   // };
-  // SysexFirmwareStatus status = NORMAL;
-  int packageIndex = 0;
+  // SysExFirmwareStatus status = NORMAL;
+public:
+  uint16_t packageIndex = 0;
   uint8_t* buffer = NULL;
   uint32_t size;
   uint32_t index;
   uint32_t crc;
-
+  bool ready;
 public:
   void clear(){
-    // free(buffer);
     buffer = NULL;
     index = 0;
     packageIndex = 0;
+    ready = false;
+    crc = -1;
   }
 
-  int error(int code){
+  uint32_t getChecksum(){
+    return crc;
+  }
+
+  bool isReady(){
+    return ready;
+  }
+
+  int setError(const char* msg){
+    error(PROGRAM_ERROR, msg);
     clear();
-    return code;
+    return -1;
   }
 
   uint8_t* getData(){
@@ -49,51 +64,68 @@ public:
     return result;
   }
 
-  int32_t handleFirmwareUpload(uint8_t* data, uint16_t length){
-    int offset = 3;
-    int idx = decodeInt(data+offset);
-    offset += 5;
-    if(idx == 0){
-      clear();
-      // first package
-      if(length < 3+5+5)
-	return error(-1);
-      // stop running program and free its memory
-      exitProgram();
-      // get firmware data size (decoded)
-      size = decodeInt(data+offset);
-      offset += 5; // it takes five 7-bit values to encode four bytes
-      // allocate memory
-      if(size > MAX_SYSEX_FIRMWARE_SIZE)
-	return error(-2);
-      buffer = (uint8_t*)EXTRAM;
-      return 0;
-    }
-    if(++packageIndex != idx)
-      return error(-7); // out of sequence package
-    int len = floor((length-offset)*7/8.0f);
-    // wait for program to exit before writing to buffer
-    if(index+len < size){
-      // mid package
-      len = sysex_to_data(data+offset, buffer+index, length-offset);
-      index += len;
-      return 0;
-    }
-    // last package
-    len = floor((length-offset-5)*7/8.0f);
-    if(index+len != size)
-      return error(-3); // wrong size
-    len = sysex_to_data(data+offset, buffer+index, length-offset-5);
+  int32_t beginFirmwareUpload(uint8_t* data, uint16_t length, uint16_t offset){
+    clear();
+    setErrorStatus(NO_ERROR);
+    // first package
+    if(length < 3+5+5)
+      return setError("Invalid SysEx package");
+    // stop running program and free its memory
+    // program.exitProgram(true);
+    // owl.setOperationMode(LOAD_MODE);
+    // program.loadProgram(2); // load progress bar
+    // program.resetProgram(true);
+    // get firmware data size (decoded)
+    size = decodeInt(data+offset);
+    offset += 5; // it takes five 7-bit values to encode four bytes
+    // allocate memory
+    if(size > MAX_SYSEX_PAYLOAD_SIZE)
+      return setError("SysEx too big");
+#ifdef USE_EXTERNAL_RAM
+    // extern char _EXTRAM; // defined in link script
+    buffer = (uint8_t*)&_EXTRAM;
+#else
+    // required by devices with no ext mem
+    extern char _PATCHRAM;
+    buffer = (uint8_t*)&_PATCHRAM; 
+#endif
+    return 0;
+  }
+
+  int32_t receiveFirmwarePackage(uint8_t* data, uint16_t length, uint16_t offset){
+    int len = sysex_to_data(data+offset, buffer+index, length-offset);
     index += len;
-    if(index != size)
-      return error(-4); // size mismatch
+    return 0;
+  }
+
+  int32_t finishFirmwareUpload(uint8_t* data, uint16_t length, uint16_t offset){
+    // last package: package index and checksum
     // check crc
     crc = crc32(buffer, size, 0);
     // get checksum: last 4 bytes of buffer
     uint32_t checksum = decodeInt(data+length-5);
     if(crc != checksum)
-      return error(-5);
+      return setError("Invalid SysEx checksum");
+    ready = true;
     return index;
+  }
+
+  int32_t handleFirmwareUpload(uint8_t* data, uint16_t length){
+    uint16_t offset = 3;
+    uint16_t idx = decodeInt(data+offset);
+    offset += 5;
+    if(idx == 0)
+      return beginFirmwareUpload(data, length, offset);
+    if(++packageIndex != idx)
+      return setError("SysEx package out of sequence"); // out of sequence package
+    int len = floor((length-offset)*7/8.0f);
+    // wait for program to exit before writing to buffer
+    if(index+len <= size)
+      // mid package
+      return receiveFirmwarePackage(data, length, offset);
+    else if(index == size)
+      return finishFirmwareUpload(data, length, offset);
+    return setError("Invalid SysEx size"); // wrong size
   }
 };
 
